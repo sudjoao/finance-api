da291127586a3e1a162498fa8548b5aebd993966
sudjoao <isudjoao@gmail.com>
2024-03-14 21:06:09 -0300

    build: add prisma to project

---
+++ prisma/migrations/20240315000530_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "phone" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
