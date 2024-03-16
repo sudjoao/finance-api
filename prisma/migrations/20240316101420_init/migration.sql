c840d18cb65c78929a6443bf04a28b87940b87a1
sudjoao <isudjoao@gmail.com>
2024-03-16 07:54:43 -0300

    feat: change user schema

--- prisma/migrations/20240316101420_init/migration.sql
+++ prisma/migrations/20240316101420_init/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
